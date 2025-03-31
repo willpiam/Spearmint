963f74dd12bb165221c83375710832a2353ea62c
willpiam <willdoyle422@gmail.com>
2025-03-31 00:33:47 -0400

    oneshot issue

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import {
+  applyDoubleCborEncoding,
   applyParamsToScript,
   Constr,
   Data,
@@ -24,16 +25,34 @@ const emulator = new Emulator([alice]);
 const lucid = await Lucid(emulator, "Preview");
 lucid.selectWallet.fromSeed(alice.seedPhrase);
 
+{
+  // tx alice send herself 2 utxos (second utxo is automatic change)
+  const tx = await lucid
+    .newTx()
+    .pay.ToAddress(alice.address, { lovelace: 1_000_000n })
+    .complete();
+
+  const txSigned = await tx.sign.withWallet().complete();
+  await txSigned.submit();
+  emulator.awaitBlock(1);
+}
+
 // pick a utxo from alice
-const utxo = (await lucid.utxosAt(alice.address))[0];
+// const utxo = (await lucid.utxosAt(alice.address))[0];
+const utxos = await lucid.utxosAt(alice.address);
+console.log(`alice utxo count:\t${utxos.length}`);
+const utxo = utxos[0];
 const utxoRefParam = new Constr(0, [
   new Constr(0, [utxo.txHash]),
   BigInt(utxo.outputIndex),
 ]);
+console.log(`utxo tx hash:\t\t${utxo.txHash}`);
+console.log(`utxo index:\t\t${utxo.outputIndex}`);
 
 const rawValidator =
   blueprint.validators.find((v) => v.title === "sparmint.sparmint.spend")!
     .compiledCode;
+
 const parameterizedValidator = applyParamsToScript(
   rawValidator,
   [utxoRefParam],
@@ -42,19 +61,20 @@ const parameterizedValidator = applyParamsToScript(
 const validator: SpendingValidator = {
   type: "PlutusV3",
   script: parameterizedValidator,
+  // script: applyDoubleCborEncoding(parameterizedValidator),
 };
 
 const mintingPolicy: MintingPolicy = validator as MintingPolicy;
 
 const policyId = mintingPolicyToId(mintingPolicy);
-console.log(`Policy ID:\t${policyId}`);
+console.log(`Policy ID:\t\t${policyId}`);
 
 const lockAddress = validatorToAddress(
   "Preview",
   validator,
   getAddressDetails(alice.address).stakeCredential,
 );
-console.log(`Lock Address:\t${lockAddress}`);
+console.log(`Lock Address:\t\t${lockAddress}`);
 
 const tokenName = "Menthol";
 const assetName = fromText(tokenName);
@@ -62,8 +82,8 @@ const assetName = fromText(tokenName);
 const refUnit = toUnit(policyId, assetName, 100);
 const userUnit = toUnit(policyId, assetName, 222);
 
-console.log(`Ref Unit:\t${refUnit}`);
-console.log(`User Unit:\t${userUnit}`);
+console.log(`Ref Unit:\t\t${refUnit}`);
+console.log(`User Unit:\t\t${userUnit}`);
 
 const metadata = Data.fromJson({
   name: tokenName,
@@ -82,26 +102,36 @@ const MintAction = {
   Burn: Data.to(new Constr(1, [])),
 };
 
-const tx = await lucid
-  .newTx()
-  .collectFrom([utxo])
-  .mintAssets(
-    {
+try {
+  const tx = await lucid
+    .newTx()
+    .collectFrom([utxo])
+    .attach.MintingPolicy(mintingPolicy)
+    .mintAssets(
+      {
+        [refUnit]: 1n,
+        [userUnit]: 1_000n,
+      },
+      MintAction.Mint,
+    )
+    .pay.ToContract(lockAddress, {
+      kind: "inline",
+      value: datum,
+    }, {
       [refUnit]: 1n,
-      [userUnit]: 1_000n,
-    },
-    MintAction.Mint,
-  )
-  .attach.MintingPolicy(mintingPolicy)
-  .pay.ToContract(lockAddress, {
-    kind: "inline",
-    value: datum,
-  }, {
-    [refUnit]: 1n,
-  })
-  .complete();
+    })
+    .complete();
 
-console.log(tx);
+  console.log(tx);
+} catch (e) {
+  const utxos2 = await lucid.utxosAt(alice.address);
+  console.log(`alice utxo count:\t${utxos2.length}`);
+  const utxo2 = utxos2[0];
+  utxo2 === utxo
+    ? console.log("utxo is still there")
+    : console.log("utxo is gone");
+  throw e;
+}
 
 // must mint reference NFT with user tokens
 
